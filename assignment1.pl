2508618daad77ccbf8dda7f8c3fb59eea2f44da7
KarinaKKarinaK <karinakalicka@gmail.com>
2025-10-30 10:26:22 +0100

    Pushing latest udpates

--- assignment1.pl
+++ assignment1.pl
@@ -1,3 +1,4 @@
+#  EXERCISE 1 ==========================================================
 fantasy_novel(lord_Of_the_Rings).
 film_trilogy(lord_Of_the_Rings).
 wrriten_by(j_r_r_Tolkien, lord_Of_the_Rings).
@@ -8,3 +9,9 @@ fantasy_writer(j_r_r_Tolkien).
 # false otherwise.)
 corruptionCheck(X, false) :- possesses_one_ring(X), \+ pure_heart(X).
 corruptionCheck(X, true) :- \+ possesses_one_ring(X).
+
+# A being who resists corruption may carry the Ring safely only if they do not use its power. [5
+# pts] (Write a rule with head safeBearer/2, returning true if the being can safely bear the Ring
+# and false otherwise; assume the rule is appended to the knowledge base that already includes
+# corruptionCheck/2.)
+
