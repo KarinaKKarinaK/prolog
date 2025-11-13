309bc5576210e937b06abb6c339702da5b607178
KarinaKKarinaK <karinakalicka@gmail.com>
2025-11-13 09:31:21 +0100

    Exercise 1 and 2 of 1

--- tests2.pl
+++ tests2.pl
@@ -1,5 +1,3 @@
-% Exercise 1
-
 bike(library, cafeteria, 3).
 bike(cafeteria, main_gate, 2).
 bike(lab_building, library, 4).
@@ -7,8 +5,9 @@ walk(main_gate, admin_office, 5).
 walk(library, lecture_hall, 6).
 walk(lecture_hall, lab_building, 7).
 
-% Exercise 1.1
 route_by_bike(F, T) :- bike(F, T, _).
 
-% Exercise 1.2
 route(F, T) :- bike(F, T, _) ; walk(F, T, _).
+
+% route(library, lecture_hall).
+% route(library, main_gate).
